dc6ec06da805825b153d55801cd8d97e6b7c6269
sabin <sabinshrestha8888@gmail.com>
2023-01-12 02:14:36 +0545

    Added booking CRUD in BookingController

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\BookCarRequest;
+use App\Http\Requests\UpdateBookCarRequest;
 use App\Http\Resources\BookingResource;
 use App\Models\Booking;
+use App\Models\Car;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use PhpParser\Node\Expr\Cast\Bool_;
 use Symfony\Component\HttpFoundation\Response;
+use TheSeer\Tokenizer\Exception;
 
 class BookingController extends Controller
 {
@@ -20,31 +24,66 @@ class BookingController extends Controller
             ['car_id', $validatedBookCar['car_id']],
         ])->first();
 
-        if (!$booking) {
-            $validatedBookCarResult = [...$validatedBookCar, 'status' => true, 'user_id' => auth()->user()->id];
-
-            $bookedCar = Booking::create($validatedBookCarResult);
-
+        if ($booking) {
             return response([
-                'Booking' => new BookingResource($bookedCar)
-            ], Response::HTTP_CREATED);
+                'message' => 'Car already booked'
+            ], Response::HTTP_BAD_REQUEST);
         }
 
+        $bookedFrom = new Carbon($validatedBookCar['booked_to']);
+
+        $bookedTo = new Carbon($validatedBookCar['booked_from']);
+
+        $bookingHour = (int) $bookedFrom->diff($bookedTo)->format('%H');
+
+        $car = Car::find($validatedBookCar['car_id']);
+
+        if (!$car) {
+            return response([
+                'message' => 'Car not found'
+            ], Response::HTTP_BAD_REQUEST);
+        }
+
+        $validatedBookCarResult = [
+            ...$validatedBookCar,
+            'status' => true,
+            'user_id' => auth()->user()->id,
+            'total_price' => $bookingHour * $car->price
+        ];
+
+        $bookedCar = Booking::create($validatedBookCarResult);
+
         return response([
-            'message' => 'Car already booked'
-        ], Response::HTTP_BAD_REQUEST);
+            'Booking' => new BookingResource($bookedCar)
+        ], Response::HTTP_CREATED);
     }
 
-    public function bookingHour()
+    public function showBookings()
     {
-        $booking = Booking::where('status', true)->first();
+        $bookings = Booking::where('id', auth()->user()->id)->first();
 
-        $bookingHours = strtotime($booking->booked_to) - strtotime($booking->booked_from);
+        return response([
+            'bookings' => new BookingResource($bookings)
+        ]);
+    }
 
-        $calculate = (int) (($bookingHours / (1000 * 60)) % 24) / 60;
+    public function cancelBooking($id)
+    {
+        Booking::find($id)->delete();
 
-        // dd($booking->car()->first()->price);
+        return response(null, Response::HTTP_NO_CONTENT);
+    }
 
-        return round($calculate * $booking->car()->first()->price, 2);
+    public function updateBooking(UpdateBookCarRequest $request)
+    {
+        $validatedUpdateBooking = $request->validated();
+
+        $booking = Booking::where('id', $request->id)->first();
+
+        $booking->update($validatedUpdateBooking);
+
+        return response([
+            'booking' => new BookingResource($booking)
+        ]);
     }
 }
